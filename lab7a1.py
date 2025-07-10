ba84dbad7916b7a4415edc2f7ae33e2a5df6a5c0
Shazma <shazma15hussain@gmail.com>
2025-07-10 12:55:32 -0400

    Update lab7a1.py

--- lab7a1.py
+++ lab7a1.py
@@ -1,17 +1,35 @@
 #!/usr/bin/env python3
-# Student ID: [seneca_id]
-from lab7a import *
-t1 = Time(8,0,0)
-t2 = Time(8,55,0)
-t3 = Time(9,50,0)
+# Student ID: Shazma Hussain
 
-td = Time(0,50,0)
+class Time:
+    """Simple object type for time of the day.
+       data attributes: hour, minute, second
+    """
+    def __init__(self, hour=12, minute=0, second=0):
+        self.hour = hour
+        self.minute = minute
+        self.second = second
 
-tsum1 = sum_times(t1,td)
-tsum2 = sum_times(t2,td)
-tsum3 = sum_times(t3,td)   
+def format_time(t):
+    return f'{t.hour:02d}:{t.minute:02d}:{t.second:02d}'
 
-ft = format_time
-print(ft(t1),'+',ft(td),'-->',ft(tsum1))
-print(ft(t2),'+',ft(td),'-->',ft(tsum2))
-print(ft(t3),'+',ft(td),'-->',ft(tsum3))
+def sum_times(t1, t2):
+    sum = Time(0, 0, 0)
+    sum.hour = t1.hour + t2.hour
+    sum.minute = t1.minute + t2.minute
+    sum.second = t1.second + t2.second
+
+    while sum.second >= 60:
+        sum.second -= 60
+        sum.minute += 1
+    while sum.minute >= 60:
+        sum.minute -= 60
+        sum.hour += 1
+    return sum
+
+def valid_time(t):
+    if t.hour < 0 or t.minute < 0 or t.second < 0:
+        return False
+    if t.minute >= 60 or t.second >= 60 or t.hour >= 24:
+        return False
+    return True
